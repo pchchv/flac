48c2f3c677c1c4136e4d16ef15e782d8b805cbf0
pchchv <ipchchv@gmail.com>
2024-11-14 13:51:13 +0300

    Implement a String method

--- meta/meta.go
+++ meta/meta.go
@@ -1,4 +1,36 @@
 package meta
 
+// Metadata block body types.
+const (
+	TypeStreamInfo    Type = 0
+	TypePadding       Type = 1
+	TypeApplication   Type = 2
+	TypeSeekTable     Type = 3
+	TypeVorbisComment Type = 4
+	TypeCueSheet      Type = 5
+	TypePicture       Type = 6
+)
+
 // Type represents the type of a metadata block body.
 type Type uint8
+
+func (t Type) String() string {
+	switch t {
+	case TypeStreamInfo:
+		return "stream info"
+	case TypePadding:
+		return "padding"
+	case TypeApplication:
+		return "application"
+	case TypeSeekTable:
+		return "seek table"
+	case TypeVorbisComment:
+		return "vorbis comment"
+	case TypeCueSheet:
+		return "cue sheet"
+	case TypePicture:
+		return "picture"
+	default:
+		return "<unknown block type>"
+	}
+}
