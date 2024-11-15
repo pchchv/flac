28f9d8bf1cc571b4165874f0ded7017fdf994aed
pchchv <ipchchv@gmail.com>
2024-11-15 15:22:46 +0300

    Create a SeekPoint type

---
+++ meta/seektable.go
@@ -0,0 +1,15 @@
+package meta
+
+// A SeekPoint specifies the byte offset and
+// initial sample number of a given target frame.
+type SeekPoint struct {
+	// Sample number of the first sample in the target frame,
+	// or 0xFFFFFFFFFFFFFFFF for a placeholder point.
+	SampleNum uint64
+	// Offset in bytes from the first byte of
+	// the first frame header to the first byte of
+	// the target frame's header.
+	Offset uint64
+	// Number of samples in the target frame.
+	NSamples uint16
+}
