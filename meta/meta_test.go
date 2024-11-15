b07741d1e33c75569a731ec26341a87ccaa5568a
pchchv <ipchchv@gmail.com>
2024-11-15 16:35:37 +0300

    Create test of a picture parsing

--- meta/meta_test.go
+++ meta/meta_test.go
@@ -1,14 +1,41 @@
 package meta_test
 
 import (
+	"bytes"
+	"os"
 	"testing"
 
 	"github.com/pchchv/flac"
+	"github.com/pchchv/flac/meta"
 )
 
 func TestMissingValue(t *testing.T) {
 	_, err := flac.ParseFile("testdata/missing-value.flac")
 	if err.Error() != `meta.Block.parseVorbisComment: unable to locate '=' in vector "title 2"` {
 		t.Fatal(err)
+	}
+}
+
+func TestParsePicture(t *testing.T) {
+	stream, err := flac.ParseFile("testdata/silence.flac")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer stream.Close()
+
+	want, err := os.ReadFile("testdata/silence.jpg")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, block := range stream.Blocks {
+		if block.Type == meta.TypePicture {
+			pic := block.Body.(*meta.Picture)
+			got := pic.Data
+			if !bytes.Equal(got, want) {
+				t.Errorf("picture data differ; expected %v, got %v", want, got)
+			}
+			break
+		}
 	}
 }
