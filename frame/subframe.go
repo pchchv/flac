2cc846e3670b969157a03ce85d57810bf26201ce
pchchv <ipchchv@gmail.com>
2024-11-21 11:38:36 +0300

    Implement a SubHeader type

--- frame/subframe.go
+++ frame/subframe.go
@@ -25,3 +25,23 @@ type RiceSubframe struct {
 	// Rice partitions.
 	Partitions []RicePartition
 }
+
+// SubHeader specifies the prediction method and order of a subframe.
+type SubHeader struct {
+	// Specifies the prediction method used to encode the audio sample of the subframe.
+	Pred Pred
+	// Prediction order used by fixed and FIR linear prediction decoding.
+	Order int
+	// Wasted bits-per-sample.
+	Wasted uint
+	// Residual coding method used by fixed and FIR linear prediction decoding.
+	ResidualCodingMethod ResidualCodingMethod
+	// Coefficients' precision in bits used by FIR linear prediction decoding.
+	CoeffPrec uint
+	// Predictor coefficient shift needed in bits used by FIR linear prediction decoding.
+	CoeffShift int32
+	// Predictor coefficients used by FIR linear prediction decoding.
+	Coeffs []int32
+	// Rice-coding subframe fields used by residual coding methods rice1 and rice2; nil if unused.
+	RiceSubframe *RiceSubframe
+}
