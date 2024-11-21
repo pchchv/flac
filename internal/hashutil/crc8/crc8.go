fa46c8d09b6bc58cfeb6cef4ece9f4d18f6836ff
pchchv <ipchchv@gmail.com>
2024-11-21 12:27:34 +0300

    Create a type digest

--- internal/hashutil/crc8/crc8.go
+++ internal/hashutil/crc8/crc8.go
@@ -1,5 +1,11 @@
 package crc8
 
+// digest represents the partial evaluation of a checksum.
+type digest struct {
+	crc   uint8
+	table *Table
+}
+
 // Table is a 256-word table representing
 // the polynomial for efficient processing.
 type Table [256]uint8
