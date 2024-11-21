47f5a1e3c359f9bcf60e2c53cb97ff5d08f91779
pchchv <ipchchv@gmail.com>
2024-11-21 11:36:45 +0300

    Create a RicePartition and a RiceSubframe types

--- frame/subframe.go
+++ frame/subframe.go
@@ -3,3 +3,22 @@ package frame
 // Pred specifies the prediction method used to encode
 // the audio samples of a subframe.
 type Pred uint8
+
+// RicePartition is a partition containing
+// a subset of the residuals of a subframe.
+type RicePartition struct {
+	// Rice parameter.
+	Param uint
+	// Residual sample size in bits-per-sample used by escaped partitions.
+	EscapedBitsPerSample uint
+}
+
+// RiceSubframe holds rice-coding subframe fields used
+// by residual coding methods rice1 and rice2.
+type RiceSubframe struct {
+	// Partition order used by fixed and FIR linear prediction decoding
+	// (for residual coding methods, rice1 and rice2).
+	PartOrder int
+	// Rice partitions.
+	Partitions []RicePartition
+}
