ae68154ae864fb0daa559f6c19e5b65d1c6ead09
pchchv <ipchchv@gmail.com>
2024-11-21 12:29:18 +0300

    Implement Size, BlockSize and Reset methods

--- internal/hashutil/crc8/crc8.go
+++ internal/hashutil/crc8/crc8.go
@@ -1,9 +1,24 @@
 package crc8
 
+// Size of a CRC-8 checksum in bytes.
+const Size = 1
+
 // digest represents the partial evaluation of a checksum.
 type digest struct {
 	crc   uint8
 	table *Table
+}
+
+func (d *digest) Size() int {
+	return Size
+}
+
+func (d *digest) BlockSize() int {
+	return 1
+}
+
+func (d *digest) Reset() {
+	d.crc = 0
 }
 
 // Table is a 256-word table representing
