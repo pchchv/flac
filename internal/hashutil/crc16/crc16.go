2bcff5742bfbf04f843e7908e6bbf609acaf22df
pchchv <ipchchv@gmail.com>
2024-11-22 12:10:00 +0300

    Create a type digest

--- internal/hashutil/crc16/crc16.go
+++ internal/hashutil/crc16/crc16.go
@@ -3,3 +3,9 @@ package crc16
 // Table is a 256-word table representing the
 // polynomial for efficient processing.
 type Table [256]uint16
+
+// digest represents the partial evaluation of a checksum.
+type digest struct {
+	crc   uint16
+	table *Table
+}
