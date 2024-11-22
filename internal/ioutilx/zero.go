d60a24cf67c75bc0f32b69384fa7a1b89808b439
pchchv <ipchchv@gmail.com>
2024-11-22 11:54:53 +0300

    Implement a Read method

--- internal/ioutilx/zero.go
+++ internal/ioutilx/zero.go
@@ -5,3 +5,12 @@ var Zero zero
 
 // zero is io.Reader which always reads zero bytes.
 type zero struct{}
+
+// Read reads len(b) zero bytes into b.
+// It returns the number of bytes read and a nil error value.
+func (zero) Read(b []byte) (n int, err error) {
+	for i := range b {
+		b[i] = 0
+	}
+	return len(b), nil
+}
