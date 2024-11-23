f1f4f07add1dadb61e1f72349c3ce6d0d3589505
pchchv <ipchchv@gmail.com>
2024-11-23 11:39:58 +0300

    Create test of a NewReadSeekerSize func

--- internal/bufseekio/readseeker_test.go
+++ internal/bufseekio/readseeker_test.go
@@ -11,3 +11,23 @@ func TestNewReadSeeker(t *testing.T) {
 		t.Fatalf("want %d got %d", defaultBufSize, len(rs.buf))
 	}
 }
+
+func TestNewReadSeekerSize(t *testing.T) {
+	buf := bytes.NewReader(make([]byte, 100))
+
+	// test custom buffer size
+	if rs := NewReadSeekerSize(buf, 20); len(rs.buf) != 20 {
+		t.Fatalf("want %d got %d", 20, len(rs.buf))
+	}
+
+	// test too small buffer size
+	if rs := NewReadSeekerSize(buf, 1); len(rs.buf) != minReadBufferSize {
+		t.Fatalf("want %d got %d", minReadBufferSize, len(rs.buf))
+	}
+
+	// test reuse existing ReadSeeker
+	rs := NewReadSeekerSize(buf, 20)
+	if rs2 := NewReadSeekerSize(rs, 5); rs != rs2 {
+		t.Fatal("expected ReadSeeker to be reused but got a different ReadSeeker")
+	}
+}
