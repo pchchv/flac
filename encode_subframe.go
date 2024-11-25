4f17c88d72df84c540ebd203e82d1f90d63f4536
pchchv <ipchchv@gmail.com>
2024-11-25 13:12:14 +0300

    Implement a getLPCResiduals function

--- encode_subframe.go
+++ encode_subframe.go
@@ -44,3 +44,35 @@ func encodeVerbatimSamples(bw *bitio.Writer, hdr frame.Header, subframe *frame.S
 
 	return nil
 }
+
+// getLPCResiduals returns the residuals
+// (signal errors of the prediction)
+// between the given audio samples and the LPC predicted audio samples,
+// using the coefficients of a given polynomial,
+// and a couple (order of polynomial;
+// i.e. len(coeffs)) of unencoded warm-up samples.
+func getLPCResiduals(subframe *frame.Subframe, coeffs []int32, shift int32) ([]int32, error) {
+	if len(coeffs) != subframe.Order {
+		return nil, fmt.Errorf("getLPCResiduals: prediction order (%d) differs from number of coefficients (%d)", subframe.Order, len(coeffs))
+	}
+
+	if shift < 0 {
+		return nil, fmt.Errorf("getLPCResiduals: invalid negative shift")
+	}
+
+	if subframe.NSamples != len(subframe.Samples) {
+		return nil, fmt.Errorf("getLPCResiduals: subframe sample count mismatch; expected %d, got %d", subframe.NSamples, len(subframe.Samples))
+	}
+
+	var residuals []int32
+	for i := subframe.Order; i < subframe.NSamples; i++ {
+		var sample int64
+		for j, c := range coeffs {
+			sample += int64(c) * int64(subframe.Samples[i-j-1])
+		}
+		residual := subframe.Samples[i] - int32(sample>>uint(shift))
+		residuals = append(residuals, residual)
+	}
+
+	return residuals, nil
+}
